de841b31a9e91a5248e8f0feff2f2cd12c5b1415
Karbou <201307916+karbou12@users.noreply.github.com>
2025-08-23 17:48:24 +0900

    Copied Dottie FlipFlow original QMK/VIA files from https://github.com/MogmaProducts/qmk_firmware.

---
+++ keyboards/mogma_products/dottie_flipflow/config.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#define MATRIX_ROWS 4
+#define MATRIX_COLS 12
+
+#define BOOTMAGIC_ROW 3
+#define BOOTMAGIC_COLUMN 5
+
+#define I2C1_SCL_PIN GP13
+#define I2C1_SDA_PIN GP12
+#define I2C_DRIVER I2CD0
+#define F_SCL 100000
---
+++ keyboards/mogma_products/dottie_flipflow/rules.mk
@@ -0,0 +1,5 @@
+CUSTOM_MATRIX = lite
+I2C_DRIVER_REQUIRED = yes
+SRC += matrix.c
+CONSOLE_ENABLE = yes
+VIA_ENABLE = yes
