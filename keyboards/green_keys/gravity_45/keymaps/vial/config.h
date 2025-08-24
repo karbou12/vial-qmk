5202ad176050a3ea3e55e9d5a52fb7b636128398
Karbou <201307916+karbou12@users.noreply.github.com>
2025-08-24 16:02:48 +0900

    Added Gravity45 vial files.

---
+++ keyboards/green_keys/gravity_45/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+// Copyright 2025 takashicompany (@takashicompany)
+// SPDX-License-Identifier: GPL-2.0-or-later
+#pragma once
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 9
+#define VIAL_KEYBOARD_UID {0x6B, 0x7B, 0xD1, 0x10, 0x4E, 0x90, 0xAE, 0x6F}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
+#define VIAL_UNLOCK_COMBO_COLS {1, 11}
