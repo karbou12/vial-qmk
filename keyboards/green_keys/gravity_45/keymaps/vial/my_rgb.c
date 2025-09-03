df9bd6762ce42fb7596eade01e17c33f40c4c8e7
Karbou <201307916+karbou12@users.noreply.github.com>
2025-09-03 21:43:25 +0900

    Fixed to set RGB LED properly when caps word become inactive for USE_LAYER_SEGMENT case.

--- keyboards/green_keys/gravity_45/keymaps/vial/my_rgb.c
+++ keyboards/green_keys/gravity_45/keymaps/vial/my_rgb.c
@@ -140,7 +140,7 @@ layer_state_t default_layer_state_set_user(layer_state_t state) {
 
 layer_state_t layer_state_set_user(layer_state_t state) {
 #ifdef USE_LAYER_SEGMENT
-    if (get_highest_layer(state) == 0) {
+    if (get_highest_layer(default_layer_state) == 0 && !is_caps_word_on()) {
         record_current_rgblight();
     }
 
