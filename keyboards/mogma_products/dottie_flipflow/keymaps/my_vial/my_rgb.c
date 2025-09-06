60a6a35760ed80ec6e7367cf1975a334dcb3378c
Karbou <201307916+karbou12@users.noreply.github.com>
2025-09-06 22:33:28 +0900

    Modified keymap as home row mods, and added layers for Mac.

--- keyboards/mogma_products/dottie_flipflow/keymaps/my_vial/my_rgb.c
+++ keyboards/mogma_products/dottie_flipflow/keymaps/my_vial/my_rgb.c
@@ -3,18 +3,26 @@
 static hsv_t my_hsv;
 static uint8_t my_mode = 0;
 
-const rgblight_segment_t PROGMEM my_layer0_layer[] = RGBLIGHT_LAYER_SEGMENTS({0, 1, HSV_CYAN});
-const rgblight_segment_t PROGMEM my_layer1_layer[] = RGBLIGHT_LAYER_SEGMENTS({0, 1, HSV_PURPLE});
-const rgblight_segment_t PROGMEM my_layer2_layer[] = RGBLIGHT_LAYER_SEGMENTS({0, 1, HSV_ORANGE});
-const rgblight_segment_t PROGMEM my_layer3_layer[] = RGBLIGHT_LAYER_SEGMENTS({0, 1, HSV_SPRINGGREEN});
+const rgblight_segment_t PROGMEM my_win_layer0_layer[] = RGBLIGHT_LAYER_SEGMENTS({0, 1, HSV_CYAN});
+const rgblight_segment_t PROGMEM my_mac_layer0_layer[] = RGBLIGHT_LAYER_SEGMENTS({0, 1, HSV_CYAN});
+const rgblight_segment_t PROGMEM my_win_layer1_layer[] = RGBLIGHT_LAYER_SEGMENTS({0, 1, HSV_PURPLE});
+const rgblight_segment_t PROGMEM my_mac_layer1_layer[] = RGBLIGHT_LAYER_SEGMENTS({0, 1, HSV_PURPLE});
+const rgblight_segment_t PROGMEM my_win_layer2_layer[] = RGBLIGHT_LAYER_SEGMENTS({0, 1, HSV_ORANGE});
+const rgblight_segment_t PROGMEM my_mac_layer2_layer[] = RGBLIGHT_LAYER_SEGMENTS({0, 1, HSV_ORANGE});
+const rgblight_segment_t PROGMEM my_win_layer3_layer[] = RGBLIGHT_LAYER_SEGMENTS({0, 1, HSV_SPRINGGREEN});
+const rgblight_segment_t PROGMEM my_mac_layer3_layer[] = RGBLIGHT_LAYER_SEGMENTS({0, 1, HSV_SPRINGGREEN});
 const rgblight_segment_t PROGMEM my_capsword_layer[] = RGBLIGHT_LAYER_SEGMENTS({0, 1, HSV_RED});
 const rgblight_segment_t PROGMEM my_layerOFF_layer[] = RGBLIGHT_LAYER_SEGMENTS({0, 1, HSV_OFF});
 
 const rgblight_segment_t * const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
-    my_layer0_layer,
-    my_layer1_layer,
-    my_layer2_layer,
-    my_layer3_layer,
+    my_win_layer0_layer,
+    my_mac_layer0_layer,
+    my_win_layer1_layer,
+    my_mac_layer1_layer,
+    my_win_layer2_layer,
+    my_mac_layer2_layer,
+    my_win_layer3_layer,
+    my_mac_layer3_layer,
     my_capsword_layer
 );
 
@@ -43,13 +51,16 @@ layer_state_t default_layer_state_set_user(layer_state_t state) {
 }
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-    if (get_highest_layer(state) == 0) {
+    if (get_highest_layer(state) == 0 || get_highest_layer(state) == 1) {
         record_current_rgblight();
     }
 
-    rgblight_set_layer_state(1, layer_state_cmp(state, 1));
     rgblight_set_layer_state(2, layer_state_cmp(state, 2));
     rgblight_set_layer_state(3, layer_state_cmp(state, 3));
+    rgblight_set_layer_state(4, layer_state_cmp(state, 4));
+    rgblight_set_layer_state(5, layer_state_cmp(state, 5));
+    rgblight_set_layer_state(6, layer_state_cmp(state, 6));
+    rgblight_set_layer_state(7, layer_state_cmp(state, 7));
 
     return state;
 };
