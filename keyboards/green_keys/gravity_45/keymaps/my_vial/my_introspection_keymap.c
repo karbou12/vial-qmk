fc567b54ef80b6559ce1e8967c30465eb6fd72cf
Karbou <201307916+karbou12@users.noreply.github.com>
2025-10-13 00:29:30 +0900

    Refactored line feed.

--- keyboards/green_keys/gravity_45/keymaps/my_vial/my_introspection_keymap.c
+++ keyboards/green_keys/gravity_45/keymaps/my_vial/my_introspection_keymap.c
@@ -70,11 +70,9 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     if (!MY_EECONFIG_process_record_user(keycode, record)) {
         return false;
-    }
-    else if (!MY_RGB_process_record_user(keycode, record)) {
+    } else if (!MY_RGB_process_record_user(keycode, record)) {
         return false;
-    }
-    else if (!MY_OS_process_record_user(keycode, record)) {
+    } else if (!MY_OS_process_record_user(keycode, record)) {
         return false;
     }
     return true;
